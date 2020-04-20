fb37ab538766d675e7d91d266a11acbfa11ca4d3
Kunal Dutta <kdutta494@gmail.com>
2020-04-20 08:07:20 +0530

    fix 2

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,8 +1,4 @@
 exports.handler = function(event, context, callback) {
-    const secretContent = `
-    <h3>Welcome To The Secret Area</h3>
-    <p>Here we can tell you that the sky is <strong>blue</strong> and two plus two equals four.</p>
-    `
     
     let body
 
